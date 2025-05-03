8518c8eb351ae4ababc8d5ed1fd286cbaab2d77a
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-05-03 09:33:45 +0530

    Add @clerk/nextjs dependency and refactor AppSidebar component for improved state management and layout

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -50,77 +50,87 @@ const items = [
 ];
 
 export function AppSidebar() {
-  const { state } = useSidebar();
+  const sidebarState = useSidebar();
+  const isExpanded = sidebarState.state === "expanded";
+  console.log({ sidebarState });
 
   return (
-    <Sidebar collapsible="icon">
-      <SidebarHeader>
-        {/* <Logo /> */}
-        <div className="flex flex-row items-center gap-2 p-2">
-          <Image
-            src="/logo-black.svg"
-            alt="SmartSheets AI"
-            width={24}
-            height={24}
-          />
-          {state === "expanded" && (
-            <div className="flex flex-col">
-              <h1 className="font-semibold text-md">
-                {process.env.NEXT_PUBLIC_APP_NAME || "SmartSheets AI"}
-              </h1>
-              <span className="text-xs text-muted-foreground">
-                {process.env.NEXT_PUBLIC_APP_TAGLINE || "Made for Teachers"}
-              </span>
-            </div>
-          )}
-        </div>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <Link href={"/"}>
-                    <Home />
-                    <span>{"Home"}</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarGroup>
-          <SidebarGroupLabel>Generate</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
+    <>
+      <Sidebar collapsible="icon">
+        <SidebarHeader>
+          {/* <Logo /> */}
+          <div className="flex flex-row items-center gap-2 p-2">
+            <Image
+              src="/logo-black.svg"
+              alt="SmartSheets AI"
+              width={24}
+              height={24}
+            />
+            {isExpanded && (
+              <div className="flex flex-col">
+                <h1 className="font-semibold text-md">
+                  {process.env.NEXT_PUBLIC_APP_NAME || "SmartSheets AI"}
+                </h1>
+                <span className="text-xs text-muted-foreground">
+                  {process.env.NEXT_PUBLIC_APP_TAGLINE || "Made for Teachers"}
+                </span>
+              </div>
+            )}
+          </div>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem>
                   <SidebarMenuButton asChild>
-                    <Link href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
+                    <Link href={"/"}>
+                      <Home />
+                      <span>{"Home"}</span>
                     </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton asChild>
-              <a href={"#"}>
-                <Settings />
-                <span>{"Settings"}</span>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-        <SidebarTrigger />
-      </SidebarFooter>
-    </Sidebar>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupLabel>Generate</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <a href={"#"}>
+                  <Settings />
+                  <span>{"Settings"}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+          {sidebarState.isMobile && sidebarState.openMobile && (
+            <SidebarTrigger />
+          )}
+          {!sidebarState.isMobile && <SidebarTrigger />}
+        </SidebarFooter>
+      </Sidebar>
+      {sidebarState.isMobile && !sidebarState.openMobile && (
+        <SidebarTrigger className="z-[5000000] fixed bottom-2 left-2" />
+      )}
+    </>
   );
 }
